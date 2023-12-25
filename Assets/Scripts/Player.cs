5ab39ffe5ef1b5cf6d55d5be597a4fa2d1f60599
MuhammetMelihKIR <muhammetmelihkr@gmail.com>
2023-12-25 22:19:36 +0300

    Interact Actions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,29 @@ public class Player : MonoBehaviour
    [SerializeField] private LayerMask countersLayerMask;
    private bool isWalking;
    private Vector3 lastInteractDir;
+
+   private void Start()
+   {
+      gameInput.OnInteractAction += GameInputOnOnInteractAction;  
+   }
+   private void GameInputOnOnInteractAction(object sender, EventArgs e)
+   {
+      Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+      Vector3 moveDir = new Vector3(inputVector.x,0f,inputVector.y);
+
+      if (moveDir != Vector3.zero) {
+         lastInteractDir= moveDir;
+      }
+      
+      float interactDistance = 2f;
+      if (Physics.Raycast(transform.position, lastInteractDir,out RaycastHit raycastHit ,interactDistance,countersLayerMask))
+      {
+         if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+            clearCounter.Interact();
+         }
+      }
+   }
+
    private void Update()
    {
      HandleMovement();
@@ -34,7 +58,7 @@ public class Player : MonoBehaviour
       if (Physics.Raycast(transform.position, lastInteractDir,out RaycastHit raycastHit ,interactDistance,countersLayerMask))
       {
          if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
-            clearCounter.Interact();
+            
          }
       }
       
