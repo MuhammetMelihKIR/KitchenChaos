a8ca2d0771dadb72f37238afd0329b0b8d949aa8
MuhammetMelihKIR <muhammetmelihkr@gmail.com>
2023-12-25 22:04:58 +0300

    Clear Counter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Player : MonoBehaviour
 {
    [SerializeField] private float moveSpeed = 7f;
    [SerializeField] private GameInput gameInput;
+   [SerializeField] private LayerMask countersLayerMask;
    private bool isWalking;
+   private Vector3 lastInteractDir;
    private void Update()
+   {
+     HandleMovement();
+     HandleInteractions();
+   }
+   
+   public bool IsWalking()
+   {
+      return isWalking;
+   }
+
+   private void HandleInteractions()
+   {
+      Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+      Vector3 moveDir = new Vector3(inputVector.x,0f,inputVector.y);
+
+      if (moveDir != Vector3.zero) {
+         lastInteractDir= moveDir;
+      }
+      
+      float interactDistance = 2f;
+      if (Physics.Raycast(transform.position, lastInteractDir,out RaycastHit raycastHit ,interactDistance,countersLayerMask))
+      {
+         if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+            clearCounter.Interact();
+         }
+      }
+      
+   }
+   private void HandleMovement()
    {
       Vector2 inputVector = gameInput.GetMovementVectorNormalized();
       Vector3 moveDir = new Vector3(inputVector.x,0f,inputVector.y);
@@ -56,9 +88,4 @@ public class Player : MonoBehaviour
       transform.forward = Vector3.Slerp(transform.forward,moveDir,Time.deltaTime * rotateSpeed);
 
    }
-   
-   public bool IsWalking()
-   {
-      return isWalking;
-   }  
 }
